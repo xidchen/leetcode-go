6e3a1141427a3af04936c83b6a70b0298ac2b6ab
Sheldon Chen <xidchen.work@gmail.com>
2024-02-26 09:19:03 +0800

    Add problem 5

--- leetcode.go
+++ leetcode.go
@@ -31,3 +31,39 @@ func (l Leetcode) lengthOfLongestSubstring(s string) int {
 	}
 	return longest
 }
+
+// 5: /problems/longest-palindromic-substring/
+func (l Leetcode) longestPalindromeSubstring(s string) string {
+	if len(s) < 2 || isPalindromicString(s) {
+		return s
+	}
+	var start, ml = -1, 0
+	for i := 0; i < len(s); i++ {
+		var odd, even string
+		if i-ml-1 >= 0 {
+			odd = s[i-ml-1 : i+1]
+		}
+		if i-ml >= 0 {
+			even = s[i-ml : i+1]
+		}
+		if len(odd) > 0 && isPalindromicString(odd) {
+			start = i - ml - 1
+			ml += 2
+			continue
+		}
+		if len(even) > 0 && isPalindromicString(even) {
+			start = i - ml
+			ml += 1
+		}
+	}
+	return s[start : start+ml]
+}
+
+func isPalindromicString(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import "fmt"
 func main() {
 	twoSum()
 	lengthOfLongestSubstring()
+	longestPalindromeSubstring()
 }
 
+// 1: /problems/two-sum/
 func twoSum() {
 	nums := []int{2, 7, 11, 15}
 	target := 9
@@ -14,8 +16,16 @@ func twoSum() {
 	fmt.Println("Two sum:", result)
 }
 
+// 3: /problems/longest-substring-without-repeating-characters/
 func lengthOfLongestSubstring() {
 	s := "abcdefabc"
 	result := Leetcode{}.lengthOfLongestSubstring(s)
 	fmt.Println("Length of longest substring:", result)
+}
+
+// 5: /problems/longest-palindromic-substring/
+func longestPalindromeSubstring() {
+	s := "babad"
+	result := Leetcode{}.longestPalindromeSubstring(s)
+	fmt.Println("Longest palindromic substring:", result)
 }
